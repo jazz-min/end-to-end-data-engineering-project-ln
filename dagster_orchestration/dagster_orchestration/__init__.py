05da3bff0d74475b9f1a455000786647bfd19f8e
jazz-min <achu.elite@gmail.com>
2024-05-03 15:31:03 -0700

    dagster scheduling changes

--- dagster_orchestration/dagster_orchestration/__init__.py
+++ dagster_orchestration/dagster_orchestration/__init__.py
@@ -1,9 +1,17 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import Definitions,AssetSelection,ScheduleDefinition, load_assets_from_modules,define_asset_job
 
 from .assets import resources
 
+big_star_job = define_asset_job("big_star_job",selection=AssetSelection.all())
+
+big_star_schedule = ScheduleDefinition(
+    job = big_star_job,
+    cron_schedule="0 * * * *", #every hour
+)
 
 defs = Definitions(
     assets=load_assets_from_modules([assets]),
-    resources=resources
+    resources=resources,
+    jobs = (big_star_job),
+    schedules=big_star_schedule
 )
